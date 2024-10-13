45ee389c0864d35d7b4d1a51a40e92ea40621c86
patelkrupal <patelkrupal@users.noreply.github.com>
2024-10-13 17:58:23 +0000

    deploy: 7dc3c281af2d3db0326af2ce7dbdf02cdc3cec01

--- pageobjects/checkoutCompletePage.js
+++
@@ -1,5 +0,0 @@
-export const title = '.title'
-export const completeHeader = 'text=Thank you for your order!'
-export const completeText = '.complete-text'
-export const ponyExpressImage = "img[alt='Pony Express']"
-export const backHomeButton = "[data-test='back-to-products']"
